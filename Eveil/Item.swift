920ec0704f68a89af36c6092b6e8126141feda66
Kush Mirchandani <kmirchandani2006@gmail.com>
2026-03-27 21:25:05 -0400

    Initial Commit

---
+++ Eveil/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Eveil
+//
+//  Created by Kush Mirchandani on 3/27/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
